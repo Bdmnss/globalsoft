5b1f83bc0a1b08819517c441b7e0a99d9ef21d9e
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2025-06-26 16:01:30 +0400

    Fix PageProps error

--- app/products/categories/[category]/page.tsx
+++ app/products/categories/[category]/page.tsx
@@ -1,12 +1,14 @@
 import ProductsClient from '../../client'
 import { getCategories } from '@/app/api/products'
+import type { PageProps } from '@/.next/types/app/products/categories/[category]/page'
 
-export default async function CategoryPage({ params }) {
+export default async function CategoryPage(props: PageProps) {
+  const params = await props.params
   const categories = await getCategories()
   return (
     <ProductsClient
       categories={categories}
-      initialCategory={params.category || 'All'}
+      initialCategory={params?.category || 'All'}
     />
   )
 }
